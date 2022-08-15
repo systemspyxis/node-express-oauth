642e8133535bb49b1991fdd3ffa6e3346da0bda2
James Osanya <JOsanya@sybrin.co.za>
2022-08-15 13:44:24 +0300

    m3

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const axios = require("axios").default
 const { randomString, timeout } = require("./utils")
+const url = require('url');
 
 const config = {
 	port: 9000,
@@ -23,9 +24,39 @@ app.use(timeout)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-/*
-Your code here
-*/
+app.get('/authorize', (req, res) => {
+	state = randomString()
+	let urlred = new URL(config.authorizationEndpoint)
+	urlred.searchParams.append('response_type', 'code')
+	urlred.searchParams.append('client_id', config.clientId)
+	urlred.searchParams.append('redirect_uri', config.redirectUri)
+	urlred.searchParams.append('scope', 'permission:name permission:date_of_birth')
+	urlred.searchParams.append('state', state)
+	res.redirect(urlred.href)
+})
+
+app.get('/callback', (req, res) => {
+	if (req.query.state === state) {
+		axios({
+			method: 'post',
+			url: config.tokenEndpoint,
+			auth: { username: config.clientId, password: config.clientSecret },
+			data:{code:req.query.code}
+		})
+			.then(function (response) {
+				axios({
+					method: 'get',
+					url: config.userInfoEndpoint,
+					headers:{authorization:"bearer "+response.data.access_token}
+				  })
+					.then(function (response) {
+						res.render("welcome",{user:response.data})
+					});
+			});
+	} else {
+		res.status(403).end()
+	}
+})
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
